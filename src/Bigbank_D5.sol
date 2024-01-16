754b2ba90bf9cef385fcdb034ad92b05b1dd5183
leacent song <leason316@foxmail.com>
2024-01-17 00:03:23 +0800

    feat: big bank

--- src/Bigbank_D5.sol
+++ src/Bigbank_D5.sol
@@ -1,3 +1,20 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
+
+import "./Bank_D4.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BigBank is Bank, Ownable {
+    modifier onlyValidAmount() {
+        require(msg.value > 0.001 ether, "Deposit amount must be greater than 0.001 ether");
+        _;
+    }
+
+    function deposit() external payable onlyValidAmount {
+        super.deposit();
+    }
+
+    function withdraw(uint256 amount) external onlyOwner {
+        super.withdraw(amount);
+    }
+}
